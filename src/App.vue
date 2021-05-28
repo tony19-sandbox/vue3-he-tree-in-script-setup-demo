2937923b82aa02a7c8160c76776ee442ff4c3c46
Tony Trinh <tony19@gmail.com>
2021-05-28 09:35:37 -0500

    add he-tree-demo in script setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Tree :value="treeData">
+    <template v-slot="{ node, path, tree }">
+      <span>
+        <b @click="tree.toggleFold(node, path)">
+          {{node.$folded ? '+' : '-'}}
+        </b>
+        {{node.text}}
+      </span>
+    </template>
+  </Tree>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+/* eslint-disable */
+import 'he-tree-vue/dist/he-tree-vue.css'
+import {Tree as HeTree, Fold} from 'he-tree-vue'
+const Tree = HeTree.mixPlugins([Fold])
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const treeData = [{text: 'node 1'}, {text: 'node 2', children: [{text: 'node 2-1'}]}]
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
